093345c1d65cb76b71eeeaafa2c5ced7ba796de5
Waris-Khan <k.waris14@gmail.com>
2020-08-11 18:42:21 +0500

    Updates

---
+++ precache-manifest.4b8d3600ed1fac74f5275ee6eac79247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e97f789874a5ce54ecc30eed21962de",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "5e8cff63a4bad900aedf",
+    "url": "/todo-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ba9e7f3f173090641860",
+    "url": "/todo-list/static/css/main.7f3da578.chunk.css"
+  },
+  {
+    "revision": "5e8cff63a4bad900aedf",
+    "url": "/todo-list/static/js/2.0a5b4a98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.0a5b4a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9e7f3f173090641860",
+    "url": "/todo-list/static/js/main.9f2affa0.chunk.js"
+  },
+  {
+    "revision": "b8f023c4114e4cc4c614",
+    "url": "/todo-list/static/js/runtime-main.2918b0a5.js"
+  }
+]);
